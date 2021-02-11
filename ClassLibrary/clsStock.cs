cd4d7740e20a5f073214e069f3ed2741d10fb516
Isaac Zerououl <P2557506@my365.dmu.ac.uk>
2021-02-11 21:05:36 +0000

    Created properties for table design: -Properties added in clsStock -Property tests added in tstStock

--- ClassLibrary/clsStock.cs
+++ ClassLibrary/clsStock.cs
@@ -1,6 +1,12 @@
 ﻿namespace ClassLibrary
 {
     public class clsStock
-    {
+    { 
+        public bool Available { get; set; }
+        public string SneakerDescription { get; set; }
+        public string SneakerColour { get; set; }
+        public int SneakerSize { get; set; }
+        public double Price { get; set; }
+        public int SneakerId { get; set; }
     }
 }
--- Testing2/tstStock.cs
+++ Testing2/tstStock.cs
@@ -15,5 +15,80 @@ namespace TestingStock
             //Test if instance exists
             Assert.IsNotNull(AStockUnit);
         }
+
+        public void ActivePropertyOk()
+        {
+            //create an instance of the class we want to create
+            clsStock AStockUnit = new clsStock();
+            // create test data to assign to property
+            Boolean TestData = true;
+            //Assign data to property
+            AStockUnit.Available = TestData;
+            //Test to see if they are equal
+            Assert.AreEqual(AStockUnit.Available, TestData);
+
+
+       }
+        public void SneakerDescriptionPropertyOk()
+        {
+            //Create instance of class
+            clsStock AStockUnit = new clsStock();
+            //create test data to assign property
+            string TestData = "New Black Air Force 1's";
+            //Assign data to property
+            AStockUnit.SneakerDescription = TestData;
+            //Test to see if values are equal
+            Assert.AreEqual(AStockUnit.SneakerDescription, TestData);
+
+        }
+        
+        public void SneakerColourPropertyOk()
+        {
+            //Create instance of class
+            clsStock AStockUnit = new clsStock();
+            //Create test data
+            string TestData = "Red";
+            //Assign test data to property
+            AStockUnit.SneakerColour = TestData;
+            //Test to see if two values are equal
+            Assert.AreEqual(AStockUnit.SneakerColour, TestData);
+        }
+
+        public void SneakerSizePropertyOk()
+        {
+            //Create instance of class
+            clsStock AStockUnit = new clsStock();
+            //Create test data 
+            int TestData = 9;
+            //Assign test data to property
+            AStockUnit.SneakerSize = TestData;
+            //Test if values are equal
+            Assert.AreEqual(AStockUnit.SneakerSize, TestData);
+        }
+ 
+        public void PricePropertyOk()
+        {
+            //Create an instance of class
+            clsStock AStockUnit = new clsStock();
+            //Create test data 
+            double TestData = 149.99;
+            //Assign data to property
+            AStockUnit.Price = TestData;
+            //Test if values are equal
+            Assert.AreEqual(AStockUnit.Price, TestData);
+
+        }
+        public void SneakerIdPropertyOk()
+        {
+            //Create instance of a class
+            clsStock AStockUnit = new clsStock();
+            //Create testdata
+            int TestData = 1;
+            //Assign data to property
+            AStockUnit.SneakerId = TestData;
+            //Test if values are equal
+            Assert.AreEqual(AStockUnit.SneakerId, TestData);
+            
+        }
     }
 }
