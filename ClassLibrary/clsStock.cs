0375959dd3213271995ded287a770e5488223c75
Isaac Zerououl <P2557506@my365.dmu.ac.uk>
2021-02-26 22:41:55 +0000

    Finished Find Method for Data Layer -Added all data members for properties in clsStock -Added all tests for find method in tstStock

--- ClassLibrary/clsStock.cs
+++ ClassLibrary/clsStock.cs
@@ -1,14 +1,89 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace ClassLibrary
 {
     public class clsStock
     { 
-        public bool Available { get; set; }
-        public string SneakerDescription { get; set; }
-        public string SneakerColour { get; set; }
-        public int SneakerSize { get; set; }
-        public double Price { get; set; }
+        //private data member for available
+        private Boolean mAvailable;
+        //public property for availbale
+        public bool Available
+        {
+            get
+            {
+                //return the private data
+                return mAvailable;
+            }
+            set
+            {
+                mAvailable = value;
+            }
+        }
+        //private data member for sneaker description
+        private string mSneakerDescription;
+        //public data member for sneaker description
+        public string SneakerDescription
+        {
+            get
+            {
+                //return the private data
+                return mSneakerDescription; 
+            }
+            set
+            {
+                mSneakerDescription = value;
+            }
+        }
+        //private data member for sneaker colour
+        private string mSneakerColour;
+        //public data member for sneaker colour
+        public string SneakerColour 
+        {
+            get
+            {
+                //return the private data
+                return mSneakerColour; 
+            }
+            set
+            {
+                mSneakerColour = value;
+            }
+        }
+
+        //private data member for sneaker description
+        private int mSneakerSize;
+        //public data member for sneaker description
+        public int SneakerSize
+        {
+            get
+            {
+                //return the private data
+                return mSneakerSize;
+            }
+            set
+            {
+                mSneakerSize = value;
+            }
+        }
+
+        //private data member for sneaker description
+        private double mPrice;
+        //public data member for sneaker description
+        public double Price
+        {
+            get
+            {
+                //return the private data
+                return mPrice;
+            }
+            set
+            {
+                mPrice = value;
+            }
+        }
         public int SneakerId
         {
             get
@@ -28,6 +103,17 @@ namespace ClassLibrary
         {
             //set private data member to test data value
             mSneakerId = 3;
+
+            mSneakerDescription = "Test Description";
+
+            mSneakerColour = "Red";
+
+            mSneakerSize = 8;
+
+            mPrice = 124.99;
+
+            mAvailable = true;
+
             //always return true
             return true;
         }
